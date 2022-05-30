31e56d957efdfab184b0a3016a53c41d6f754ca6
Dries Augustyns <augustyns.dries@gmail.com>
2022-05-30 17:03:30 +0200

    Fix: Cleaned up typings closes #1

--- src/types.ts
+++ src/types.ts
@@ -1,138 +1,67 @@
+export interface CData {
+  _cdata: string;
+}
+
+export interface Text {
+  _text: string;
+}
+
 export interface Root {
   _declaration: Declaration;
   rss: Rss;
 }
 
 export interface Declaration {
-  _attributes: Attributes;
-}
-
-export interface Attributes {
-  version: string;
-  encoding: string;
+  _attributes: {
+    version: string;
+    encoding: string;
+  };
 }
 
 export interface Rss {
-  _attributes: Attributes2;
-  channel: Channel;
+  _attributes: {
+    'xmlns:dc': string;
+    'xmlns:content': string;
+    'xmlns:atom': string;
+    version: string;
+    'xmlns:cc': string;
+  };
+  channel: Articles;
 }
 
-export interface Attributes2 {
-  'xmlns:dc': string;
-  'xmlns:content': string;
-  'xmlns:atom': string;
-  version: string;
-  'xmlns:cc': string;
-}
-
-export interface Channel {
-  title: Title;
-  description: Description;
-  link: Link;
+export interface Articles {
+  title: CData;
+  description: CData;
+  link: Text;
   image: Image;
-  generator: Generator;
-  lastBuildDate: LastBuildDate;
+  generator: Text;
+  lastBuildDate: Text;
   'atom:link': AtomLink[];
-  webMaster: WebMaster;
-  item: Item[];
-}
-
-export interface Title {
-  _cdata: string;
-}
-
-export interface Description {
-  _cdata: string;
-}
-
-export interface Link {
-  _text: string;
+  webMaster: CData;
+  item: ArticleItem[];
 }
 
 export interface Image {
-  url: Url;
-  title: Title2;
-  link: Link2;
-}
-
-export interface Url {
-  _text: string;
-}
-
-export interface Title2 {
-  _text: string;
-}
-
-export interface Link2 {
-  _text: string;
-}
-
-export interface Generator {
-  _text: string;
-}
-
-export interface LastBuildDate {
-  _text: string;
+  url: Text;
+  title: Text;
+  link: Text;
 }
 
 export interface AtomLink {
-  _attributes: Attributes3;
+  _attributes: {
+    href: string;
+    rel: string;
+    type?: string;
+  };
 }
 
-export interface Attributes3 {
-  href: string;
-  rel: string;
-  type?: string;
-}
-
-export interface WebMaster {
-  _cdata: string;
-}
-
-export interface Item {
-  title: Title3;
-  description: Description2;
-  link: Link3;
-  guid: Guid;
-  category: Category[];
-  'dc:creator': DcCreator;
-  pubDate: PubDate;
-  'atom:updated': AtomUpdated;
-}
-
-export interface Title3 {
-  _cdata: string;
-}
-
-export interface Description2 {
-  _cdata: string;
-}
-
-export interface Link3 {
-  _text: string;
-}
-
-export interface Guid {
-  _attributes: Attributes4;
-  _text: string;
-}
-
-export interface Attributes4 {
-  isPermaLink: string;
-}
-
-export interface Category {
-  _cdata: string;
-}
-
-export interface DcCreator {
-  _cdata: string;
-}
-
-export interface PubDate {
-  _text: string;
-}
-
-export interface AtomUpdated {
-  _text: string;
+export interface ArticleItem {
+  title: CData;
+  description: CData;
+  link: Text;
+  guid: Text;
+  category: CData[];
+  'dc:creator': CData;
+  pubDate: Text;
+  'atom:updated': Text;
 }
